54d63ceef71396aafa0e01d05c05eb6ef2c33da9
Mayur Navsupe <mnavsupe@tibco.com>
2018-10-27 17:13:38 +0530

    GET sap api call: working GET method call Signed-off-by: Mayur Navsupe <mnavsupe@tibco.com>

--- getMethodTest.go
+++ getMethodTest.go
@@ -1,32 +1,21 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
 func main() {
-	//responce, err := http.ServerContextKey("B6NePYsFuQRE9REfQiUCdtwcGANYOVsb")
-	//http.Request.api.ServerContextKey:= "B6NePYsFuQRE9REfQiUCdtwcGANYOVsb"
-	//http.Header.Set("APIkey", "B6NePYsFuQRE9REfQiUCdtwcGANYOVsb", "")
-	//http.Header.Set.APIkey :=  "B6NePYsFuQRE9REfQiUCdtwcGANYOVsb"
-	//https://sandbox.api.sap.com/successfactors/odata/v2/Background_Community?%24top=1
-	responce, err := http.Get("http://httpbin.org/get")
-	if err != nil {
-		fmt.Println("There is some error as %s \n", err)
-	} else {
-		data, _ := ioutil.ReadAll(responce.Body)
-		fmt.Println(string(data))
-	}
-
-	jsonData := map[string]string{"name": "NICE"}
-	jsonValue, _ := json.Marshal(jsonData)
-	request, err := http.NewRequest("GET", "https://sandbox.api.sap.com/successfactors/odata/v2/Background_Community?%24top=1", bytes.NewBuffer(jsonValue))
+	client := &http.Client{}
+	request, err := http.NewRequest("GET", "https://sandbox.api.sap.com/successfactors/odata/v2/Background_Community?%24top=1", nil)
 	request.Header.Set("APIkey", "B6NePYsFuQRE9REfQiUCdtwcGANYOVsb")
-	data1, _ := ioutil.ReadAll(request.Body)
+	response, err := client.Do(request)
+
+	data1, _ := ioutil.ReadAll(response.Body)
 	fmt.Println(string(data1))
+	if err != nil {
+		fmt.Print(err)
+	}
 
 }
