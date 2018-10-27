94c0f3f95e6d49c5665925737645f9a82630b7e2
Mayur Navsupe <mnavsupe@tibco.com>
2018-10-27 15:12:21 +0530

    added another function Signed-off-by: Mayur Navsupe <mnavsupe@tibco.com>

--- webapp.go
+++ webapp.go
@@ -10,7 +10,14 @@ func index_handler(w http.ResponseWriter, r *http.Request) {
 
 	//http://localhost:999 will show this line in a web browser
 }
+func about_handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is how it change")
+
+	//http://localhost:999/abouut/ will show this line in a web browser
+}
+
 func main() {
 	http.HandleFunc("/", index_handler)
+	http.HandleFunc("/abouut/", about_handler)
 	http.ListenAndServe(":999", nil)
 }
