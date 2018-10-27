5ef22e17b91ac9e4013c1ecc8277c6beaf8eabeb
Mayur Navsupe <mnavsupe@tibco.com>
2018-10-27 15:21:06 +0530

    changes function name

--- webapp.go
+++ webapp.go
@@ -5,19 +5,19 @@ import (
 	"net/http"
 )
 
-func index_handler(w http.ResponseWriter, r *http.Request) {
+func indexhandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Who Go is nit")
 
 	//http://localhost:999 will show this line in a web browser
 }
-func about_handler(w http.ResponseWriter, r *http.Request) {
+func abouthandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is how it change")
 
 	//http://localhost:999/abouut/ will show this line in a web browser
 }
 
 func main() {
-	http.HandleFunc("/", index_handler)
-	http.HandleFunc("/abouut/", about_handler)
+	http.HandleFunc("/", indexhandler)
+	http.HandleFunc("/abouut/", abouthandler)
 	http.ListenAndServe(":999", nil)
 }
